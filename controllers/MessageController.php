80ad8bd5c46d321dc7d3cd3afcf8930a983920e7
abirmes <abir.meskini02@gmail.com>
2025-02-13 18:21:23 +0100

    page messages

---
+++ controllers/MessageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\controllers;
+
+use App\core\Application;
+use App\models\Message;
+use App\models\User;
+
+class MessageController
+{
+    private Message $messageModel;
+    private User $userModel;
+
+
+    public function __construct()
+    {
+        $this->messageModel = new Message();
+        $this->userModel = new User();
+    }
+
+    public function message($params)
+    {
+        return Application::$app->Router->renderView("projet", $params);
+    }
+
+
+
+}
